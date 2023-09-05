4feb08ae7c5008674797f1f9ee37fc35c8f7fbb0
di_uskova <di_uskova@betboom.org>
2023-09-05 20:31:49 +0300

    phones_viewers

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,28 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
 from phones.models import Phone
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('csv_file', type=str, help='Path to the CSV file')
 
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            slug = slugify(phone.get("name"))
+
+            new_phone = Phone(
+                name=phone.get('name'),
+                price=phone.get('price'),
+                image=phone.get('image'),
+                release_date=phone.get('release_date'),
+                lte_exists=phone.get('lte_exists'),
+                slug=slug
+            )
+            new_phone.save()
+            print(f'Imported phone successfully: {new_phone.name}')
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +8,30 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    context = {'phones': []}
+    get_sort = request.GET.get('sort')
+    phones = Phone.objects.all()
+
+    if get_sort == 'name':
+        phones = phones.order_by('name')
+    elif get_sort == 'min_price':
+        phones = phones.order_by('price')
+    elif get_sort == 'max_price':
+        phones = phones.order_by('-price')
+
+    context['phones'] = phones
+
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {"phone": {
+        "name": phone.name,
+        "image": phone.image,
+        "price": phone.price,
+        "release_date": phone.release_date,
+        "lte_exists": phone.lte_exists
+    }}
     return render(request, template, context)
