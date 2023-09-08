7006126c030e6c4b5035a7d529a4a867628bd15b
di_uskova <di_uskova@betboom.org>
2023-09-08 23:52:26 +0300

    add admin fields

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Object, Relationship, Tag
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            form.cleaned_data
+            raise ValidationError('Error')
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Relationship
+    formset = RelationshipInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+
+@admin.register(Relationship)
+class RelationshipAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -6,11 +6,42 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
 
     def __str__(self):
         return self.title
+
+
+class Object(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Объект'
+        verbose_name_plural = 'Объекты'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название тега')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Relationship(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Статья')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Тег')
+    is_main = models.BooleanField(default=False, verbose_name='Главный тег')
+
+    class Meta:
+        verbose_name = 'Связь'
+        verbose_name_plural = 'Связи'
