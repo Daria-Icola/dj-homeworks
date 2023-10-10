544551734c71bc58b692fd2c035800c748b824d8
di_uskova <di_uskova@betboom.org>
2023-10-10 11:30:20 +0300

    fix

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -23,5 +23,5 @@ class AdvertisementViewSet(ModelViewSet):
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+            return [IsAuthenticated(), IsOwner()]
         return []
